e7b9dc6ba5da367471ddcf3cb6c24f35af45f3b4
Ahmet Ari <35655833+smahmetari@users.noreply.github.com>
2026-01-11 12:17:52 +0300

    saat

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const monthNameEl = document.getElementById("month-name");
 const dayNameEl = document.getElementById("day-name");
 const dayNumEl = document.getElementById("day-num");
 const yearEl = document.getElementById("year");
+const saatEl = document.getElementById("saat");
 
 const date = new Date();
 const month = date.getMonth();
 const day = date.getDate();
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
 
 monthNameEl.innerText = date.toLocaleString("default", { month: "long" });
 dayNameEl.innerText = date.toLocaleString("default", { weekday: "long" });
 dayNumEl.innerText = date.getDate();
 yearEl.innerText = date.getFullYear();
+saatEl.innerText = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
